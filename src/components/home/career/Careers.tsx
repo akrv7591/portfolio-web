eaa4d488b1535db45603ffeefd8cdb5e73e97c14
a7591 <a7591@zeroweb.kr>
2024-01-16 15:21:44 +0900

    added unique key

--- src/components/home/career/Careers.tsx
+++ src/components/home/career/Careers.tsx
@@ -6,7 +6,7 @@ export default function Careers() {
   return careers.map(career => (
       <Stack key={career.label} gap={"50px"}>
         <Title order={2} c={"var(--mantine-primary-color-5)"}>{career.label}</Title>
-        {career.items.map(job => <JobCard job={job}/>)}
+        {career.items.map(job => <JobCard job={job} key={job.label}/>)}
       </Stack>
     )
   )
